c52d30a2b7daa149967ddf37fd079caa457b1885
1Polybius1 <146327892+1Polybius1@users.noreply.github.com>
2023-12-08 11:02:05 +0100

    Added Player + Required Locomotion Scripts

---
+++ Assets/Scripts/PlayerScripts/InputManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    private Keyboard keyboard;
+    
+    public bool interactKeyPressed;
+    
+    
+    
+    
+    void Start()
+    {
+        keyboard = Keyboard.current;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
