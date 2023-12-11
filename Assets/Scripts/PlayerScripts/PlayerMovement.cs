1289fbf12f162238488c53329887850b22b858ba
Benjameepmeep <benjamin.arctander@gmail.com>
2023-12-12 00:26:56 +0100

    Updates to Player

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Configurable Parameters")]
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 2.5f;
 
     private PlayerControls _controls;
 
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnDisable() => _controls.Disable();
     
+    // TODO: Update the Update() (general playerMovement), so that colliders on objects still work when framerate is below 60.  
     private void Update()
     {
         // Move the player
