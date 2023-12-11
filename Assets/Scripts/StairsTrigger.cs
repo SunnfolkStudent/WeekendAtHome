5e7d586464818ea8f1d37244116da2a40a8e9f4f
1Polybius1 <146327892+1Polybius1@users.noreply.github.com>
2023-12-11 17:55:18 +0100

    Started work on outdoors

--- Assets/Scripts/StairsTrigger.cs
+++ Assets/Scripts/StairsTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stairs : MonoBehaviour
+public class StairsTrigger : MonoBehaviour
 {
 
     private String currentFloor;
