22fcfc3fe122f7b4cf34ccbfc9776d7bf3ae1824
1Polybius1 <146327892+1Polybius1@users.noreply.github.com>
2023-12-11 18:30:10 +0100

    Got outdoor switch working

--- Assets/Scripts/OutdoorTrigger.cs
+++ Assets/Scripts/OutdoorTrigger.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OutdoorTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private GameObject bottomFloor;
+    
+    private String insideOrOutside;
+    
+    private GameObject[] outdoorToDespawn, outdoor;
+    
+
     void Start()
     {
+        insideOrOutside = "Inside";
         
+        outdoorToDespawn = GameObject.FindGameObjectsWithTag("Outdoor to Despawn");
+        outdoor = GameObject.FindGameObjectsWithTag("Outdoors");
+        
+        foreach(GameObject outdoorObjects in outdoor)
+            outdoorObjects.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (insideOrOutside.Equals("Inside"))
+        {
+            bottomFloor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.38f);
+            
+            foreach(GameObject outdoorToDespawnObjects in outdoorToDespawn)
+                outdoorToDespawnObjects.SetActive(false);
+            
+            foreach(GameObject outdoorObjects in outdoor)
+                outdoorObjects.SetActive(true);
+            
+            insideOrOutside = "Outside";
+        }
+        else
+        {
+            bottomFloor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            
+            foreach(GameObject outdoorToDespawnObjects in outdoorToDespawn)
+                outdoorToDespawnObjects.SetActive(true);
+            
+            foreach(GameObject outdoorObjects in outdoor)
+                outdoorObjects.SetActive(false);
+            
+            insideOrOutside = "Inside";
+        }
+
+        return;
     }
 }
