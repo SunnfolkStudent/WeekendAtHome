4ab3a59659e3c26fb8fe7342fef82f7b16d4db76
Benjameepmeep <benjamin.arctander@gmail.com>
2023-12-13 14:17:20 +0100

    Animations fixed - thanks to Markus : )

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -18,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
     private static readonly int Vertical = Animator.StringToHash("Vertical");
     private static readonly int Speed = Animator.StringToHash("Speed");
+    
+    
 
     private void Update()
     {
@@ -26,10 +28,23 @@ public class PlayerMovement : MonoBehaviour
         // Move the player
         // transform.Translate(_controls.Player.Movement.ReadValue<Vector2>() * (moveSpeed * Time.deltaTime));
         // Movement = _controls.Player.Movement.ReadValue<Vector2>();
+
+        if (_movement == Vector2.zero)
+        {
+            anim.Play("Player_Idle_Normal");
+        }
+        else
+        {
+            anim.Play("Movement");
+            anim.SetFloat("Horizontal", _movement.x);
+            anim.SetFloat("Vertical", _movement.y);
+        }
         
-        anim.SetFloat(Horizontal, _movement.x);
-        anim.SetFloat(Vertical, _movement.y);
-        anim.SetFloat(Speed, _movement.sqrMagnitude);
+        if (_movement.x != 0)
+        {
+           anim.transform.localScale = new Vector3(-_movement.x, 1f, 1f);
+        }
+     
     }
 
     private void FixedUpdate()
