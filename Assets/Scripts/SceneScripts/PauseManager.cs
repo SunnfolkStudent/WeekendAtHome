ee4c8ab4284aefae6402911f205056506193cbdd
Benjameepmeep <benjamin.arctander@gmail.com>
2023-12-17 00:51:31 +0100

    Updated Tilemap TopFloor + Sorted Bugs

--- Assets/Scripts/SceneScripts/PauseManager.cs
+++ Assets/Scripts/SceneScripts/PauseManager.cs
@@ -1,50 +1,52 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseManager : MonoBehaviour
+namespace SceneScripts
 {
+    public class PauseManager : MonoBehaviour
+    {
 
-    // Declare Variables 
-    private PlayerControls controls;
+        // Declare Variables 
+        private PlayerControls controls;
 
-    public static bool pauseSceneIsActive = false;
+        public static bool pauseSceneIsActive = false;
     
-    // Initialize the controls
-    private void Awake() => controls = new PlayerControls();
-    private void OnEnable() => controls.Enable();
-    private void OnDisable() => controls.Disable();
+        // Initialize the controls
+        private void Awake() => controls = new PlayerControls();
+        private void OnEnable() => controls.Enable();
+        private void OnDisable() => controls.Disable();
 
-    public void UnloadPauseScene()
-    {
-        // Unload the pause scene
-        SceneManager.UnloadSceneAsync("PauseOverlayScene");
-        return;
-    }
-
-    public void SetPauseSceneIsActiveToInactive()
-    {
-        // Resume the time
-        Time.timeScale = 1f;
-        pauseSceneIsActive = false;
-        GameObject.FindGameObjectWithTag("Player").GetComponent<AudioListener>().enabled = true;
-        return;
-    }
-    
-    void Update()
-    {
-        // Check if escape is pressed
-        if (controls.Pause.OpenMenu.triggered)
+        public void UnloadPauseScene()
         {
-            if (!pauseSceneIsActive)
+            // Unload the pause scene
+            SceneManager.UnloadSceneAsync("PauseOverlayScene");
+            return;
+        }
+
+        public void SetPauseSceneIsActiveToInactive()
+        {
+            // Resume the time
+            Time.timeScale = 1f;
+            pauseSceneIsActive = false;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<AudioListener>().enabled = true;
+            GameObject.FindGameObjectWithTag("EventSystemMain").SetActive(true);
+            return;
+        }
+    
+        void Update()
+        {
+            // Check if escape is pressed
+            if (controls.Pause.OpenMenu.triggered)
             {
-                // Stops time and adds the pause scene
-                Time.timeScale = 0f;
-                SceneManager.LoadScene("PauseOverlayScene", LoadSceneMode.Additive);
-                pauseSceneIsActive = true;
-                GameObject.FindGameObjectWithTag("Player").GetComponent<AudioListener>().enabled = false;
+                if (!pauseSceneIsActive)
+                {
+                    // Stops time and adds the pause scene
+                    Time.timeScale = 0f;
+                    SceneManager.LoadScene("PauseOverlayScene", LoadSceneMode.Additive);
+                    pauseSceneIsActive = true;
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<AudioListener>().enabled = false;
+                    GameObject.FindGameObjectWithTag("EventSystemMain").SetActive(false);
+                }
             }
         }
     }
