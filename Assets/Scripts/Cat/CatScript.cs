7f3d6fb4e1bce6258bb0603a9c24a205ed7b05d6
Benjameepmeep <benjamin.arctander@gmail.com>
2023-12-21 13:35:07 +0100

    Fixes to GlassDoor and Cat

--- Assets/Scripts/Cat/CatScript.cs
+++ Assets/Scripts/Cat/CatScript.cs
@@ -1,114 +1,115 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using Pathfinding;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public class CatAnimation : MonoBehaviour
+namespace Cat
 {
-    public AIPath aiPath;
-    [FormerlySerializedAs("animator")] public Animator anim;
-    public GameObject goal;
-    public Vector2[] goalSpots;
-    public bool[] staysForRandom;
-    public float[] howLongToStay;
-    public string[] whichAnimationToPlay;
-    public AudioClip[] whatSoundToPlay;
-    private int _randomGoal;
-    private float _timeBetweenMeows = 0;
-    private bool _destinationReached;
-    private bool _goingTowardsCatFood;
+    public class CatAnimation : MonoBehaviour
+    {
+        public AIPath aiPath;
+        [FormerlySerializedAs("animator")] public Animator anim;
+        public GameObject goal;
+        public Vector2[] goalSpots;
+        public bool[] staysForRandom;
+        public float[] howLongToStay;
+        public string[] whichAnimationToPlay;
+        public AudioClip[] whatSoundToPlay;
+        private int _randomGoal;
+        private float _timeBetweenMeows = 0;
+        private bool _destinationReached;
+        private bool _goingTowardsCatFood;
     
 
-    public AudioSource meows;
+        public AudioSource meows;
     
-    private bool onTheMove = true;
-    private void FixedUpdate()
-    {
-        //Finds Which Direction The Cat is Generally Moving
-        if (onTheMove)
+        private bool onTheMove = true;
+        private void FixedUpdate()
         {
-            if (aiPath.desiredVelocity == Vector3.zero)
+            //Finds Which Direction The Cat is Generally Moving
+            if (onTheMove)
             {
-                //anim.Play("Player_Idle_Normal");
-            }
-            else
-            {
-                anim.Play("Movement");
-                anim.SetFloat("Horizontal", aiPath.desiredVelocity.x);
-                anim.SetFloat("Vertical", aiPath.desiredVelocity.y);
-            }
+                if (aiPath.desiredVelocity == Vector3.zero)
+                {
+                    //anim.Play("Player_Idle_Normal");
+                }
+                else
+                {
+                    anim.Play("Movement");
+                    anim.SetFloat("Horizontal", aiPath.desiredVelocity.x);
+                    anim.SetFloat("Vertical", aiPath.desiredVelocity.y);
+                }
             
-            if (aiPath.desiredVelocity.x != 0 && aiPath.desiredVelocity.y == 0)
-            {
-                anim.transform.localScale = new Vector3(-aiPath.desiredVelocity.x, 1f, 1f);
-            }
-            else
-            {
-                anim.transform.localScale = new Vector3(1f, 1f, 1f);
-            }
+                if (aiPath.desiredVelocity.x != 0 && aiPath.desiredVelocity.y == 0)
+                {
+                    anim.transform.localScale = new Vector3(-aiPath.desiredVelocity.x, 1f, 1f);
+                }
+                else
+                {
+                    anim.transform.localScale = new Vector3(1f, 1f, 1f);
+                }
 
-            //If At Destination. Play Goal Function
-            if (aiPath.reachedDestination && !_destinationReached)
-            {
-                _destinationReached = true;
-                Debug.Log(_randomGoal);
-                anim.Play("Player_Idle_Normal");
-                onTheMove = false;
-                //Debug.Log("Reached Destination");
-                OnGoalEnter();
-            }
+                //If At Destination. Play Goal Function
+                if (aiPath.reachedDestination && !_destinationReached)
+                {
+                    _destinationReached = true;
+                    Debug.Log(_randomGoal);
+                    anim.Play("Player_Idle_Normal");
+                    onTheMove = false;
+                    //Debug.Log("Reached Destination");
+                    OnGoalEnter();
+                }
             
-            //Counter For Random Occasional Meows
-            if (_timeBetweenMeows > 0)
-            {
-                _timeBetweenMeows -= Time.deltaTime;
-            }
-            else
-            {
-                //meows.Play();
-                Debug.Log("Meow");
-                _timeBetweenMeows = Random.Range(5, 20);
-            }
+                //Counter For Random Occasional Meows
+                if (_timeBetweenMeows > 0)
+                {
+                    _timeBetweenMeows -= Time.deltaTime;
+                }
+                else
+                {
+                    //meows.Play();
+                    Debug.Log("Meow");
+                    _timeBetweenMeows = Random.Range(5, 20);
+                }
             
+            }
+
+            if (CatFoodFull.catBowlFull)
+            {
+                _goingTowardsCatFood = true;
+                goal.transform.position = new Vector3(1,0,0);
+            }
         }
 
-        if (CatFoodFull.catBowlFull)
+        //Plays When At Goal, Sets A New Goal, Then Starts Coroutine
+        private void OnGoalEnter()
         {
-            _goingTowardsCatFood = true;
-            goal.transform.position = new Vector3(1,0,0);
+            StartCoroutine(WaitForNewGoal());
         }
-    }
 
-    //Plays When At Goal, Sets A New Goal, Then Starts Coroutine
-    private void OnGoalEnter()
-    {
-        StartCoroutine(WaitForNewGoal());
-    }
-
-    //Waits For Seconds, Then Starts New Goal
-    IEnumerator WaitForNewGoal()
-    {
-        yield return new WaitForSeconds(howLongToStay[_randomGoal]);
-        if (staysForRandom[_randomGoal])
+        //Waits For Seconds, Then Starts New Goal
+        IEnumerator WaitForNewGoal()
         {
-            Debug.Log("Random Time Added");
-            yield return new WaitForSeconds(Random.Range(1,15));
-        }
+            yield return new WaitForSeconds(howLongToStay[_randomGoal]);
+            if (staysForRandom[_randomGoal])
+            {
+                Debug.Log("Random Time Added");
+                yield return new WaitForSeconds(Random.Range(1,15));
+            }
 
-        if (_goingTowardsCatFood)
-        {
-            yield return new WaitForSeconds(13);
-            CatFoodFull.catBowlFull = false;
-            _goingTowardsCatFood = false;
+            if (_goingTowardsCatFood)
+            {
+                yield return new WaitForSeconds(13);
+                CatFoodFull.catBowlFull = false;
+                _goingTowardsCatFood = false;
+            }
+            _randomGoal = Random.Range(0, goalSpots.Length);
+            Debug.Log(goalSpots[_randomGoal]);
+            goal.transform.position = goalSpots[_randomGoal];
+            Debug.Log(goal.transform.position);
+            onTheMove = true;
+            _destinationReached = false;
         }
-        _randomGoal = Random.Range(0, goalSpots.Length);
-        Debug.Log(goalSpots[_randomGoal]);
-        goal.transform.position = goalSpots[_randomGoal];
-        Debug.Log(goal.transform.position);
-        onTheMove = true;
-        _destinationReached = false;
     }
 }
--- Assets/Scripts/Triggers/OutdoorTrigger.cs
+++ Assets/Scripts/Triggers/OutdoorTrigger.cs
@@ -1,97 +1,98 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-public class OutdoorTrigger : MonoBehaviour
+namespace Triggers
 {
-    // Declare Variables
-    [SerializeField] private GameObject bottomFloor;
-    
-    private String insideOrOutside;
-    private GameObject[] outdoorToDespawn, outdoor;
-
-    [SerializeField] private float transparencyValue, moveAmount, playerOutdoorLightValue;
-
-    private float playerLightIntensity;
-
-    private GameObject player;
-
-    [SerializeField] private FootstepManager footstepManager;
-    
-    void Start()
+    public class OutdoorTrigger : MonoBehaviour
     {
-        // Fetch all of the objects with the Outdoor and OutdoorToDespawn tags and store them in a list
-        // and hide all of the outdoor objects
-        
-        insideOrOutside = "Inside";
-        footstepManager.isOutside = false;
-        
-        outdoorToDespawn = GameObject.FindGameObjectsWithTag("Outdoor to Despawn");
-        outdoor = GameObject.FindGameObjectsWithTag("Outdoors");
-        
-        player = GameObject.FindWithTag("Player");
-        
-        playerLightIntensity = player.GetComponentInChildren<Light2D>().intensity;
-        
-        foreach(GameObject outdoorObjects in outdoor)
-            outdoorObjects.SetActive(false);
-    }
+        // Declare Variables
+        [SerializeField] private GameObject bottomFloor;
+    
+        private String insideOrOutside;
+        private GameObject[] outdoorToDespawn, outdoor;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        
-        // Ran when the player enters the outdoor trigger
-        // Checks if the player is currently inside or outside
-        // Then it activates all of the objects corresponding to where the player is + hiding objects that need to be hidden
-        // Sets the light2d child of player to an intensity of 0 and then moves down
-        
-        // Does opposite when player reenters
-        
-        if (insideOrOutside.Equals("Inside"))
-        {
-            footstepManager.isOutside = true;
-            
-            bottomFloor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, transparencyValue);
-            
-            foreach (Transform child in bottomFloor.transform)
-                child.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, transparencyValue);
-            
-            foreach(GameObject outdoorToDespawnObjects in outdoorToDespawn)
-                outdoorToDespawnObjects.SetActive(false);
-            
-            foreach(GameObject outdoorObjects in outdoor)
-                outdoorObjects.SetActive(true);
+        [SerializeField] private float transparencyValue, moveAmount, playerOutdoorLightValue;
 
-            transform.position = new Vector3(transform.position.x, transform.position.y - moveAmount);
-            
-            player.GetComponentInChildren<Light2D>().intensity = playerOutdoorLightValue;
-            player.GetComponentInChildren<SpriteRenderer>().sortingOrder = -1;
-            insideOrOutside = "Outside";
-        }
-        else
+        private float playerLightIntensity;
+
+        private GameObject player;
+
+        [SerializeField] private FootstepManager footstepManager;
+    
+        void Start()
         {
+            // Fetch all of the objects with the Outdoor and OutdoorToDespawn tags and store them in a list
+            // and hide all of the outdoor objects
+        
+            insideOrOutside = "Inside";
             footstepManager.isOutside = false;
-            
-            bottomFloor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
-            
-            foreach (Transform child in bottomFloor.transform)
-                child.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
-            
-            foreach(GameObject outdoorToDespawnObjects in outdoorToDespawn)
-                outdoorToDespawnObjects.SetActive(true);
-            
+        
+            outdoorToDespawn = GameObject.FindGameObjectsWithTag("Outdoor to Despawn");
+            outdoor = GameObject.FindGameObjectsWithTag("Outdoors");
+        
+            player = GameObject.FindWithTag("Player");
+        
+            playerLightIntensity = player.GetComponentInChildren<Light2D>().intensity;
+        
             foreach(GameObject outdoorObjects in outdoor)
                 outdoorObjects.SetActive(false);
-            
-            transform.position = new Vector3(transform.position.x, transform.position.y + moveAmount);
-            
-            player.GetComponentInChildren<Light2D>().intensity = playerLightIntensity;
-            player.GetComponentInChildren<SpriteRenderer>().sortingOrder = 50;
-            insideOrOutside = "Inside";
         }
 
-        return;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+        
+            // Ran when the player enters the outdoor trigger
+            // Checks if the player is currently inside or outside
+            // Then it activates all of the objects corresponding to where the player is + hiding objects that need to be hidden
+            // Sets the light2d child of player to an intensity of 0 and then moves down
+        
+            // Does opposite when player reenters
+        
+            if (insideOrOutside.Equals("Inside"))
+            {
+                footstepManager.isOutside = true;
+            
+                bottomFloor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, transparencyValue);
+            
+                foreach (Transform child in bottomFloor.transform)
+                    child.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, transparencyValue);
+            
+                foreach(GameObject outdoorToDespawnObjects in outdoorToDespawn)
+                    outdoorToDespawnObjects.SetActive(false);
+            
+                foreach(GameObject outdoorObjects in outdoor)
+                    outdoorObjects.SetActive(true);
+
+                transform.position = new Vector3(transform.position.x, transform.position.y - moveAmount);
+            
+                player.GetComponentInChildren<Light2D>().intensity = playerOutdoorLightValue;
+                player.GetComponentInChildren<SpriteRenderer>().sortingOrder = -1;
+                insideOrOutside = "Outside";
+            }
+            else
+            {
+                footstepManager.isOutside = false;
+            
+                bottomFloor.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            
+                foreach (Transform child in bottomFloor.transform)
+                    child.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            
+                foreach(GameObject outdoorToDespawnObjects in outdoorToDespawn)
+                    outdoorToDespawnObjects.SetActive(true);
+            
+                foreach(GameObject outdoorObjects in outdoor)
+                    outdoorObjects.SetActive(false);
+            
+                transform.position = new Vector3(transform.position.x, transform.position.y + moveAmount);
+            
+                player.GetComponentInChildren<Light2D>().intensity = playerLightIntensity;
+                player.GetComponentInChildren<SpriteRenderer>().sortingOrder = 50;
+                insideOrOutside = "Inside";
+            }
+
+            return;
+        }
     }
 }
